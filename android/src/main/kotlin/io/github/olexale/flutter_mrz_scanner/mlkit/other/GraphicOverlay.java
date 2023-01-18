e543c89c54030d0a0a731e94ca077f83fb76bb68
TPS-mobility <tpsmobilityservices@gmail.com>
2023-01-18 19:46:50 +0500

    Update GraphicOverlay.java

--- android/src/main/kotlin/io/github/olexale/flutter_mrz_scanner/mlkit/other/GraphicOverlay.java
+++ android/src/main/kotlin/io/github/olexale/flutter_mrz_scanner/mlkit/other/GraphicOverlay.java
@@ -5,7 +5,7 @@ import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
-import com.google.android.gms.vision.CameraSource;
+import io.github.olexale.mlkit.camera.CameraSource;
 
 import java.util.HashSet;
 import java.util.Set;
