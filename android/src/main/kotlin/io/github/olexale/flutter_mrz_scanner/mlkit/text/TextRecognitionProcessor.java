92af8ce6ba0a86ddcc5aa06cdcefa529228f2cea
TPS-mobility <tpsmobilityservices@gmail.com>
2023-01-18 19:58:34 +0500

    Update TextRecognitionProcessor.java

--- android/src/main/kotlin/io/github/olexale/flutter_mrz_scanner/mlkit/text/TextRecognitionProcessor.java
+++ android/src/main/kotlin/io/github/olexale/flutter_mrz_scanner/mlkit/text/TextRecognitionProcessor.java
@@ -16,7 +16,7 @@ import com.google.mlkit.common.MlKitException;
 import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
-import com.google.mlkit.vision.text.TextRecognizerOptionsInterface;
+import com.google.mlkit.vision.text.TextRecognizerOptions;
 import com.google.mlkit.vision.text.TextRecognizer;
 
 import net.sf.scuba.data.Gender;
@@ -62,7 +62,7 @@ public class TextRecognitionProcessor {
     public TextRecognitionProcessor(DocType docType, ResultListener resultListener) {
         this.docType = docType;
         this.resultListener = resultListener;
-        textRecognizer = TextRecognition.getClient(TextRecognizerOptionsInterface.LATIN);
+        textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
     }
 
     //region ----- Exposed Methods -----
